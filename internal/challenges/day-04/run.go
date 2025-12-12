98fa7fe65c44096e6e4f7cb987f3543e894d4cf4
Humza Moazzam <humza2466@icloud.com>
2025-12-12 15:03:34 +0100

    adding part two recursion

--- internal/challenges/day-04/run.go
+++ internal/challenges/day-04/run.go
@@ -12,7 +12,9 @@ func Run() {
 
 	input := helpers.LoadParsedList[Row]("./internal/challenges/day-04/input.txt")
 
-	fmt.Println("The answer to part one is:", CalculateRolls(input))
+	// fmt.Println("The answer to part one is:", CalculateRolls(input))
+	fmt.Println("The answer to part one is:", Recursion(input, false))
+	fmt.Println("The answer to part two is:", Recursion(input, true))
 	// not 1444
 
 	// for _, row := range input {
@@ -87,4 +89,77 @@ func CalculateRolls(rows []Row) int {
 	}
 
 	return res
+}
+
+func Recursion(rows []Row, partTwo bool) int {
+	sum := 0
+
+	res := 1
+
+	for res > 0 {
+		newRes, newRows := CalculateRollsFunc(rows, partTwo)
+		if !partTwo {
+			return newRes
+		}
+		rows = newRows
+		res = newRes
+		sum += res
+	}
+
+	return sum
+}
+
+func CalculateRollsFunc(rows []Row, partTwo bool) (int, []Row) {
+	res := 0
+
+	for i, row := range rows {
+		for j, val := range row {
+			if val != roll {
+				continue
+			}
+
+			count := 0
+
+			// left
+			if j > 0 && (row[j-1] == roll) {
+				count++
+			}
+			// right
+			if j < len(row)-1 && (row[j+1] == roll) {
+				count++
+			}
+			// above
+			if i > 0 && (rows[i-1][j] == roll) {
+				count++
+			}
+			// below
+			if i < len(rows)-1 && (rows[i+1][j] == roll) {
+				count++
+			}
+			// top left
+			if i > 0 && j > 0 && (rows[i-1][j-1] == roll) {
+				count++
+			}
+			// top right
+			if i > 0 && j < len(row)-1 && (rows[i-1][j+1] == roll) {
+				count++
+			}
+			// bottom left
+			if i < len(rows)-1 && j > 0 && (rows[i+1][j-1] == roll) {
+				count++
+			}
+			// bottom right
+			if i < len(rows)-1 && j < len(row)-1 && (rows[i+1][j+1] == roll) {
+				count++
+			}
+			if count < 4 {
+				res++
+				if partTwo {
+					rows[i][j] = "."
+				}
+			}
+		}
+	}
+
+	return res, rows
 }
